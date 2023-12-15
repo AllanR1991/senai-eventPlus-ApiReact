b605155083d377d7d7fa203b7240c0f66c3d5814
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-12-15 14:55:25 -0300

    exibe comentarios de eventos anteriores com filtro

--- src/pages/DetalheEvento/DetalheEvento.jsx
+++ src/pages/DetalheEvento/DetalheEvento.jsx
@@ -1,23 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext,useEffect, useState } from 'react';
 import Title from '../../components/Title/Title';
 import Container from '../../components/Container/Container';
 import MainContent from '../../components/MainContent/MainContent';
 import './DetalheEvento.css'
 import Table from "./TableDe/TableDe";
 import { useParams } from 'react-router-dom';
+import { UserContext } from "../../context/AuthContext";
+import api, {
+    eventsResource,
+    myEventsResource,
+    presencesEventResource,
+    commentaryEventResource,
+    commentaryEventIAResource,
+    eventsTypeResource,
+  } from "../../Services/Service";
 
 const DetalhesEventoPage = () => {
 
     const { idEvento } = useParams();
 
     const [evento, setEvento] = useState([])
-
-    const [comentario, setComentario] = useState([]);
-
+    const [data, setData] = useState([]);
+    const [descricao, setDescricao] = useState([]);
     const [tipo, setTipo] = useState([]);
+    const [idTipoEvento, setIdTipoEvento]= useState([]);
+    const [comentarios, setComentario] = useState([]);
+    const token = JSON.parse(localStorage.getItem('token'));
 
-    
+    const { userData } = useContext(UserContext);
 
+    //console.log(token);
+    useEffect(() => {
+        listarComentario();
+      }, []); //
+
+    async function listarComentario(){
+        
+        const getEvento = await api.get(`${eventsResource}/${idEvento}`)
+
+        const listaDeComentario = await api.get(commentaryEventResource);
+
+        const comentariosOfensasFiltrados = await listaDeComentario.data.filter(
+            (comm) => comm.idEvento === idEvento && comm.exibe === true
+        );
+        
+        const comentariosFiltrados = await listaDeComentario.data.filter(
+            (comm) => comm.idEvento === idEvento
+        );
+
+        setEvento(getEvento.data.nomeEvento);
+        setDescricao(getEvento.data.descricao);         
+        setData(getEvento.data.dataEvento);
+        //setIdTipoEvento(getEvento.data.idTipoEvento);
+        const idTeste = getEvento.data.idTipoEvento
+        const getTipoEvento = await api.get(`${eventsTypeResource}/${idTeste}`)
+
+
+        console.log(getTipoEvento.data.tipoEvento)
+
+        setTipo(getTipoEvento.data.tipoEvento);
+
+        //console.log("Tipo evento =", tipoEventoFilter)
+        userData.role == "Administrador" ?
+            setComentario(comentariosFiltrados):
+            setComentario(comentariosOfensasFiltrados);
+        
+    }
     return (
         <>
             <MainContent>
@@ -27,8 +75,10 @@ const DetalhesEventoPage = () => {
                             <Title titleText={"Detalhes Evento"} />
                             <Table
                                 evento={evento}
-                                comentario={comentario}
-                                tipo={tipo}
+                                descricao={descricao}
+                                data={data}
+                                tipoEvento={tipo}
+                                comentarios={comentarios}
                             />
 
                         </div>
--- src/pages/DetalheEvento/TableDe/TableDe.jsx
+++ src/pages/DetalheEvento/TableDe/TableDe.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './TableDe.css'
 import "react-tooltip/dist/react-tooltip.css";
-    
 
-const TableDe = ({ evento, comentario, tipo }) => {
+
+const TableDe = ({ evento, descricao, data, comentarios, tipoEvento }) => {
     return (
         <table className="table-data text-color">
             <thead className="table-data__head">
@@ -25,33 +25,34 @@ const TableDe = ({ evento, comentario, tipo }) => {
                 <tr
                     className='tbal-data__head-row tbal-data__head-row--red-color'
                 >
-                    <th className="table-data__head-title table-data__head-title--big font-color">{}</th>
-                    <th className="table-data__head-title table-data__head-title--big font-color" >{}</th>
-                    <th className="table-data__head-title table-data__head-title--big font-color">{}</th>
-                    <th className="table-data__head-title table-data__head-title--big font-color">{}</th>
+                    <th className="table-data__head-title table-data__head-title--big font-color">{evento}</th>
+                    <th className="table-data__head-title table-data__head-title--big font-color" >{descricao}</th>
+                    <th className="table-data__head-title table-data__head-title--big font-color">{tipoEvento}</th>
+                    <th className="table-data__head-title table-data__head-title--big font-color">{data}</th>
                 </tr>
             </thead>
 
             <tbody>
                 <tr className="table-data__head-row">
-                <th className="table-data__head-title table-data__head-title--big">
+                    <th className="table-data__head-title table-data__head-title--big">
                         Comentário
                     </th>
                     <th className="table-data__head-title table-data__head-title--big">
                         Usuário
                     </th>
- 
+
                 </tr>
-                {comentario.map((c)=>{
-                    if(comentario.exibe === true){
-                        return(
+                {
+
+                    comentarios.map((c) => {
+                        return (
                             <tr className="tbal-data__head-row" key={Math.random()}>
                                 <td>{c.descricao}</td>
-                                <td className="table-data__head-title table-data__head-title--big">{c.usuario.nome}</td>
+                                <td className="table-data__head-title table-data__head-title--big">{c.usuario.nome }</td>
                             </tr>
-                            )
-                    }
-                })}
+                        )
+
+                    })}
             </tbody>
 
 
